c3d05e8a7398565599abdd7231ac8dae8b0a88ec
Sergej Smelov <smelowsergey@gmail.com>
2023-10-10 22:03:52 +0500

    Add markup for the nav

---
+++ src/routes/Navigation.svelte
@@ -0,0 +1,23 @@
+<nav class="nav">
+	<ol class="nav__list">
+		<li class="nav__list-item">
+			<p class="nav__step">Step 1</p>
+			<a href="/" class="nav_link">Your info</a>
+		</li>
+
+		<li class="nav__list-item">
+			<p class="nav__step">Step 2</p>
+			<a href="/SelectPlan" class="nav_link">Select plan</a>
+		</li>
+
+		<li class="nav__list-item">
+			<p class="nav__step">Step 3</p>
+			<a href="/AddOns" class="nav_link">Add-ons</a>
+		</li>
+
+		<li class="nav__list-item">
+			<p class="nav__step">Step 4</p>
+			<a href="/Summary" class="nav_link">Summary</a>
+		</li>
+	</ol>
+</nav>

0ff7f467b69b5247d7d6f404ad6b7532c1cdc43a
Sergej Smelov <smelowsergey@gmail.com>
2023-10-10 17:44:13 +0500

    Add the markup shared between pages

---
+++ src/routes/AddOns/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import iconArrowLeft from "../lib/images/icon-arrow-left.svg"
+	import iconArrowRight from "../lib/images/icon-arrow-right.svg"
+</script>
+
+<div class="page">
+	<header class="page__header">
+		<h1 class="page__title">...</h1>
+		<p class="page__description">...</p>
+	</header>
+
+	<form class="form">...</form>
+
+	<div class="nav-arrows">
+		<a href="#0" class="nav-arrows__link">
+			<img
+				src={iconArrowLeft}
+				alt="Previous step"
+				class="nav-arrows__icon" />
+			<img
+				src={iconArrowRight}
+				alt="Next Step"
+				class="nav-arrows__icon" />
+		</a>
+	</div>
+</div>

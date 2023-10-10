ad8c6b2bbb83d32c22913b8f2542456d1af22b73
Sergej Smelov <smelowsergey@gmail.com>
2023-10-10 15:36:50 +0500

    Add marcup for the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,13 @@
-<slot />
+<script lang="ts">
+	import Navigation from "./Navigation.svelte"
+</script>
+
+<div class="app">
+	<aside class="sidebar">
+		<Navigation />
+	</aside>
+
+	<main class="card">
+		<slot />
+	</main>
+</div>

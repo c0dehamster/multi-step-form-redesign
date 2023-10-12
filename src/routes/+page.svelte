89e5a023ad04f79678743d16bed05a5890d90a9b
Sergej Smelov <smelowsergey@gmail.com>
2023-10-12 15:54:02 +0500

    Layout of the form (approx)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 		<div class="input-wrapper">
 			<label for="" class="label">Name</label>
 			<input type="text" class="input" placeholder="e.g. Stephen King" />
-			<p class="error" />
+			<p class="error">Error</p>
 		</div>
 
 		<div class="input-wrapper">
@@ -45,3 +45,42 @@
 		</a>
 	</div>
 </div>
+
+<style>
+	.page {
+		display: grid;
+		justify-items: center;
+		gap: 1.5rem;
+	}
+
+	.form {
+		width: 100%;
+		padding: 2rem;
+		display: grid;
+		gap: 1rem;
+	}
+
+	.input-wrapper {
+		width: 100%;
+
+		display: grid;
+		grid-template-areas:
+			"label error"
+			"input input";
+	}
+
+	.label {
+		grid-area: label;
+	}
+
+	.error {
+		grid-area: error;
+
+		justify-self: end;
+		text-align: end;
+	}
+
+	.input {
+		grid-area: input;
+	}
+</style>

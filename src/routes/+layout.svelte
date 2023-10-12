4d762fb932a1565932890d0bd633dd39c03bad6f
Sergej Smelov <smelowsergey@gmail.com>
2023-10-12 15:02:35 +0500

    Rougn layout of the nav (mobile / tablet)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="app">
-	<aside class="sidebar">
+	<aside class="navigation">
 		<Navigation />
 	</aside>
 
@@ -13,3 +13,27 @@
 		<slot />
 	</main>
 </div>
+
+<style>
+	.app {
+		position: relative;
+
+		width: 100%;
+
+		display: grid;
+		grid-template-areas:
+			"navigation"
+			"main";
+		justify-items: center;
+
+		/* background: var(--gradient-background-main); */
+	}
+
+	.navigation {
+		grid-area: navigation;
+	}
+
+	.card {
+		grid-area: main;
+	}
+</style>
--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,23 +1,80 @@
 <nav class="nav">
 	<ol class="nav__list">
 		<li class="nav__list-item">
-			<p class="nav__step">Step 1</p>
-			<a href="/" class="nav_link">Your info</a>
+			<a href="/" class="nav__link">
+				<div class="nav__label">
+					<p class="nav__step">Step 1</p>
+					<p class="nav__name">Your info</p>
+				</div>
+			</a>
 		</li>
 
 		<li class="nav__list-item">
-			<p class="nav__step">Step 2</p>
-			<a href="/SelectPlan" class="nav_link">Select plan</a>
+			<a href="/SelectPlan" class="nav__link">
+				<div class="nav__label">
+					<p class="nav__step">Step 2</p>
+					<p class="nav__name">Select plan</p>
+				</div>
+			</a>
 		</li>
 
 		<li class="nav__list-item">
-			<p class="nav__step">Step 3</p>
-			<a href="/AddOns" class="nav_link">Add-ons</a>
+			<a href="/AddOns" class="nav__link">
+				<div class="nav__label">
+					<p class="nav__step">Step 3</p>
+					<p class="nav__name">Add-ons</p>
+				</div>
+			</a>
 		</li>
 
 		<li class="nav__list-item">
-			<p class="nav__step">Step 4</p>
-			<a href="/Summary" class="nav_link">Summary</a>
+			<a href="/Summary" class="nav__link">
+				<div class="nav__label">
+					<p class="nav__step">Step 4</p>
+					<p class="nav__name">Summary</p>
+				</div>
+			</a>
 		</li>
 	</ol>
 </nav>
+
+<style>
+	.nav__list {
+		position: relative;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 2rem;
+
+		counter-reset: navigation;
+	}
+
+	.nav__list-item {
+		counter-increment: navigation;
+	}
+
+	.nav__link {
+		position: relative;
+
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	.nav__link::before {
+		content: counter(navigation);
+
+		font-size: var(--font-size-700);
+	}
+
+	@media screen and (width < 45rem) {
+		.nav__link::before {
+			font-size: var(--font-size-600);
+		}
+
+		.nav__label {
+			display: none;
+		}
+	}
+</style>
